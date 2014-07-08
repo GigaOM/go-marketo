9b1fc15725ed879579e4ddba0b0455c010585f75
Will Luo <will.luo@gigaom.com>
2014-07-08 16:27:07 -0700

    implemented the get_lead_by_id() and get_leads() functions, and associated unit tests

--- components/class-go-marketo-api.php
+++ components/class-go-marketo-api.php
@@ -63,4 +63,83 @@ class GO_Marketo_API
 
 		return $this->auth_token;
 	}//END get_auth_token
+
+	/**
+	 * retrieve a single lead using the Marketo id
+	 *
+	 * @param int $id Marketo id of a lead
+	 * @return array attributes of the lead if found, NULL if not
+	 */
+	public function get_lead_by_id( $id )
+	{
+		$url = go_marketo()->config( 'endpoint_url' ) . '/v1/lead/' . absint( $id ) . '.json';
+
+		$results = $this->marketo_rest_get( $url );
+
+		if ( 0 < count( $results ) )
+		{
+			return (array) $results[0]; // should just get one lead per Marketo id
+		}
+
+		return NULL;
+	}//END get_lead_by_id
+
+	/**
+	 * retrieve a list of leads filtered by attribute $type and $value
+	 *
+	 * @param string $filter_type what filter type to use. this is named
+	 *  after the lead attribute fields. e.g. 'id', 'email', 'facebookId',
+	 *  etc.
+	 * @param mixed $filter_values can be comma-separated string or an array,
+	 *  of the values of $type to filter for.
+	 * @param array $fields what fields to return for each lead
+	 */
+	public function get_leads( $filter_type, $filter_values, $fields = array() )
+	{
+		if ( is_array( $filter_values ) )
+		{
+			$filter_values = implode( ',', array_map( 'urlencode', $filter_values ) );
+		}
+		else
+		{
+			$filter_values = urlencode( $filter_values );
+		}
+
+		$url = go_marketo()->config( 'endpoint_url' ) . '/v1/leads.json?filterType=' . urlencode( $filter_type ) . '&filterValues=' . $filter_values;
+
+		if ( ! empty( $fields ) )
+		{
+			$fields = implode( ',', array_map( 'urlencode', $fields ) );
+			$url .= '&fields=' . sanitize_text_field( $fields );
+		}
+
+		return $this->marketo_rest_get( $url );
+	}//END get_leads
+
+	/**
+	 * make an HTTP GET request to Marketo's RESET API. Mainly we add the
+	 * authorization token to the HTTP header.
+	 *
+	 * @param $url string the url to request, query vars included.
+	 * @return array results from the HTTP GET request. NULL if we encountered
+	 *  an HTTP error
+	 */
+	private function marketo_rest_get( $url )
+	{
+		$response = wp_remote_get(
+			$url,
+			array(
+				'headers' => array(
+					'Authorization' => 'Bearer ' . $this->get_auth_token(),
+				),
+			)
+		);
+
+		if ( 200 != $response['response']['code'] )
+		{
+			return NULL;
+		}
+
+		return json_decode( $response['body'] )->result;
+	}//END marketo_rest_get
 }//END class
--- tests/class-go-marketo-test.php
+++ tests/class-go-marketo-test.php
@@ -17,7 +17,6 @@ class GO_Marketo_Test extends WP_UnitTestCase
 	public function setUp()
 	{
 		parent::setUp();
-		add_filter( 'go_config', array( $this, 'go_config_filter' ), 10, 2 );
 	}//END setUp
 
 	/**
@@ -31,6 +30,9 @@ class GO_Marketo_Test extends WP_UnitTestCase
 		$this->assertFalse( empty( $config ) );
 	}//END test_singleton
 
+	/**
+	 * test GO_Marketo_API's get_auth_token() function
+	 */
 	public function test_get_auth_token()
 	{
 		$token = go_marketo()->api()->get_auth_token();
@@ -38,16 +40,37 @@ class GO_Marketo_Test extends WP_UnitTestCase
 	}//END test_get_auth_token
 
 	/**
-	 * customize the system config file for our tests
+	 * test GO_Marketo_API's get_lead_by_id() function
 	 */
-	public function go_config_filter( $config, $which )
+	public function test_get_lead_by_id()
 	{
-		if ( 'go-marketo' == $which )
-		{
-///			$config = array(
-//			);
-		}//END if
+		// the id is for will.luo+test2@gigaom.com
+		// https://app-sjo.marketo.com/leadDatabase/loadLeadDetail?leadId=26059509
+		$lead = go_marketo()->api()->get_lead_by_id( 26059509 );
 
-		return $config;
-	}//END go_config_filter
+		$this->assertFalse( empty( $lead ) );
+		$this->assertTrue( 0 < count( $lead ) );
+		$this->assertEquals( 26059509, $lead['id'] );
+		$this->assertEquals( 'will.luo+test2@gigaom.com', $lead['email'] );
+	}//END test_get_lead_by_id
+
+	/**
+	 * test GO_Marketo_API's get_leads() function
+	 */
+	public function test_get_leads()
+	{
+		$leads = go_marketo()->api()->get_leads( 'Email', array( 'will.luo+test2@gigaom.com' ), array( 'firstName', 'lastName', 'email', 'wpid' ) );
+
+		$this->assertEquals( 1, count( $leads ) );
+		$this->assertEquals( 'will.luo+test2@gigaom.com', $leads[0]->email );
+		$this->assertEquals( 26059509, $leads[0]->id );
+		$this->assertEquals( 137141, $leads[0]->wpid );
+
+		$leads = go_marketo()->api()->get_leads( 'wpid', 137141, array( 'firstName', 'lastName', 'email', 'wpid' ) );
+
+		$this->assertEquals( 1, count( $leads ) );
+		$this->assertEquals( 'will.luo+test2@gigaom.com', $leads[0]->email );
+		$this->assertEquals( 26059509, $leads[0]->id );
+		$this->assertEquals( 137141, $leads[0]->wpid );
+	}//END test_get_leads
 }// END class
