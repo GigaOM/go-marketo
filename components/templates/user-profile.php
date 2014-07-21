a0e63e92b5230d0156c7e64b14f6354dc4f3e390
Will Luo <will.luo@gigaom.com>
2014-07-21 16:33:58 -0700

    a little more validation

--- components/templates/user-profile.php
+++ components/templates/user-profile.php
@@ -4,6 +4,6 @@
 		<?php echo $user_status; ?>
 	</div>
 	<button class="go-marketo-sync button-secondary sync" id="go-marketo-user-sync-btn" value="go_marketo_user_sync">Sync to Marketo</button>
-	<input type="hidden" class="user" id="go-marketo-user-sync-user" name="go_marketo_user_sync_user" value="<?php echo $user->ID; ?>" />
+	<input type="hidden" class="user" id="go-marketo-user-sync-user" name="go_marketo_user_sync_user" value="<?php echo absint( $user->ID ); ?>" />
 	<span class="feedback"></span>
 </div>
